483172fd97933cc14aa79f3686e7be1433e49844
KhaledAlabssi <khaled.absi5@gmail.com>
2020-02-29 06:44:10 +0100

    integrate blog app and test db

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+def blogs(request):
+    blogs = Blog.objects.order_by('-date')
+    return render(request, 'blog/blogs.html', {'blogs': blogs})
